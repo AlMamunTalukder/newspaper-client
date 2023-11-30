09b4cac3d83ce6d7b68cdc301b1af255073b6b4c
AlMamunTalukder <almamuntalukder0@gmail.com>
2023-11-30 23:24:00 +0600

    show all user

--- src/Pages/DashBoard/AllUsers/AllUsers.jsx
+++ src/Pages/DashBoard/AllUsers/AllUsers.jsx
@@ -1,5 +1,59 @@
+import { useEffect, useState } from "react";
+
 const AllUsers = () => {
-  return <div></div>;
+  const [allUsers, setAllUsers] = useState([]);
+  console.log(allUsers);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setAllUsers(data));
+  }, []);
+
+  return (
+    <div className="-mt-[420px] ml-56 w-96">
+      <div className="overflow-x-auto h-screen w-[1170px]">
+        <table className="table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>D.P</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allUsers.map((item, index) => (
+              /* row 1 */
+              <tr key={item._id}>
+                <th>{index + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask w-12 h-12">
+                        <img
+                          src={item.img}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <div className="font-bold">{item.name}</div>
+                </td>
+                <td>{item.email}</td>
+                <th>
+                  <button className="btn btn-success btn-md">Make Admin</button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllUsers;
