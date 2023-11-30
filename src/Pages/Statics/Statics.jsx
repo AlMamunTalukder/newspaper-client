e73f6359001a501635599a1ea3c2ebd9a446812a
AlMamunTalukder <almamuntalukder0@gmail.com>
2023-11-30 22:42:08 +0600

    user statics show

--- src/Pages/Statics/Statics.jsx
+++ src/Pages/Statics/Statics.jsx
@@ -1,24 +1,22 @@
-import { useContext } from "react";
-import { AuthContext } from "../../Authentication/Providers/AuthProvider";
+import { useEffect, useState } from "react";
+
 import CountUp from "react-countup";
 
 const Statics = () => {
-  const { user } = useContext(AuthContext);
+  const [userStatics, setUserStatics] = useState([]);
+  console.log(userStatics);
 
-  console.log(user);
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUserStatics(data));
+  }, []);
 
   return (
     <div className="card lg:card-side bg-gray-200 shadow-xl  lg:mx-32 lg:mb-20 py-10 pl-14 border-2 border-black font-bold">
       <h2 className="text-4xl font-bold text-black">Total Users: </h2>
       <div className="text-black  text-xl mt-3 ml-3 flex gap-4">
-        <CountUp end={user} duration={2} />
-        <CountUp start={0} end={100} delay={0}>
-          {({ countUpRef }) => (
-            <div>
-              <span ref={countUpRef} />
-            </div>
-          )}
-        </CountUp>
+        <CountUp end={userStatics.length} duration={2} />
       </div>
     </div>
   );
